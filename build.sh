59215382348e19665b73998915a0b1d2c6726fae
junrong_zhang <junrong_zhang@yunzhijia.com>
2023-05-31 10:21:32 +0800

    chore: build shell

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "version: " version
+if [ -z "$version" ]; then
+    echo "version is required."
+    exit 1
+fi
+
+export GOOS=windows
+export GOARCH=amd64
+go build -ldflags "-H=windowsgui" -o "builds/clock-out-reminder.$version.exe"
+
+export GOARCH=arm64
+go build -ldflags "-H=windowsgui" -o "builds/clock-out-reminder.$version.arm64.exe"
